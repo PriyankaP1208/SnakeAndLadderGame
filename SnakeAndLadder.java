103a26e4f1989fc21ceaabcf00c4be13c47476da
PriyankaP1208 <ppriyapatil1208@gmail.com>
2020-12-15 09:45:26 +0530

    Added:Repear till Player reaches the winning position 100

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -4,26 +4,32 @@ class SnakeAndLadder
 	public static void main(String[] args)
 	{
 		int position=0;
-		int diceRoll = (int)(Math.random()*6 )+1 ;
-		System.out.println(diceRoll);
-		System.out.println(diceRoll);
-		int choice;
-		Scanner sc=new Scanner(System.in);
-		System.out.println("1.No play 2.Ladder 3.Snake");
-		choice = (int)(Math.random()*3 )+1 ;
-		switch(choice)
+		while(position!=100)
 		{
-			case 1:
-					System.out.println("No Play.");
-					break;
-			case 2:
-					position+=diceRoll;
-					System.out.println("ladder");
-					break;
-			case 3:
-					position-=diceRoll;
-					System.out.println("snake");
-					break;
+			int diceRoll = (int)(Math.random()*6 )+1 ;
+			System.out.println(diceRoll);
+			System.out.println(diceRoll);
+			int choice;
+			Scanner sc=new Scanner(System.in);
+			System.out.println("1.No play 2.Ladder 3.Snake");
+			choice = (int)(Math.random()*3 )+1 ;
+			switch(choice)
+			{
+				case 1:
+						System.out.println("No Play.");
+						break;
+				case 2:
+						position+=diceRoll;
+						System.out.println("ladder");
+						break;
+				case 3:
+						System.out.println("Snake");
+						if((position-diceRoll)<0)
+							position=0;
+						else
+							position-=diceRoll;
+						break;
+			}
 		}
 	}
 }
